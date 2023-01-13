78e8eda5c3a10903d7dadeecc8d5c6c0a527cdf1
ALONSO <alon.miranda.m@gmail.com>
2023-01-12 23:17:47 -0300

    INGLES

---
+++ index.js
@@ -0,0 +1,187 @@
+
+/* Usado para generar el menu responsivo */
+const checkbox = document.querySelector('#check');
+const elUl = document.getElementById("elUl")
+
+checkbox.addEventListener('click', () => {
+  // Si el checkbox está marcado, agregamos la clase "active" al label
+  if (checkbox.checked) {
+    elUl.style.overflow = "auto"; /* overflow usado para poder scrolear en el menu responsive, */
+    document.body.style.overflow = 'hidden'; /* overflow usado para no poder scrollear la pagina body mientras esta activo el menu resposive, o mas bien mientras este con el check el checkbox */
+    
+    elUl.classList.add('active');
+  } else {
+    // Si no está marcado, quitamos la clase "active" del label
+    elUl.classList.remove('active');
+    document.body.style.overflow = 'auto';
+  }
+});
+
+
+
+
+   // Cuando se scrollea 20px por debajo de la parte superior, se muestra el boton automaticamente, de lo contrario no se ve
+ window.onscroll = function() {scrollFunction()};
+
+ function scrollFunction() {
+   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+     document.getElementById("myBtn").style.display = "block";
+   } else {
+     document.getElementById("myBtn").style.display = "none";
+   }
+}
+
+function topFunction() { /* usada unicamente para que el desplazamiento hacia la parte superior sea mas suave */
+ window.scrollTo({
+   top: 0,
+   behavior: 'smooth'
+ });
+}
+
+
+
+
+
+
+/* Lo siguiente es usado para cambiar el valor de los titulos entre ingles y español */
+const titles = ["titulo1", "titulo2", "titulo3", "titulo4", "titulo5", "titulo6"]; 
+
+for (let i = 0; i < titles.length; i++){ // se iteraran estos valores para referenciar los id de forma rapida con el for
+  const title = document.getElementById(titles[i]);
+  title.addEventListener("click", function(){ //cada titulo tendra su propio evento click
+    cambiarIdioma(titles[i]);
+  });
+} 
+// Función para cambiar entre el texto en inglés y español
+function cambiarIdioma(id) {
+  // Obtiene el elemento con el id especificado
+  const titleElement = document.getElementById(id);
+  // Obtiene el texto actual del elemento
+  const currentText = titleElement.textContent;
+  // Asigna el texto en el idioma opuesto al actual
+  const newText = (currentText === oraciones[id]["english"]) ? oraciones[id]["spanish"] : oraciones[id]["english"];
+  // Establece el nuevo texto en el elemento
+  titleElement.textContent = newText;
+
+  // Obtiene el elemento del body
+  const bodyElement = document.querySelector("body");
+  // Si el modo de la página es "light-mode" y el texto actual es en inglés
+  if (bodyElement.classList.contains("light-mode") && currentText === oraciones[id]["english"]) {
+    // Cambia el color del texto a negro
+    titleElement.style.color = "blue";
+  }
+  if (bodyElement.classList.contains("light-mode") && currentText === oraciones[id]["spanish"]) {
+    // Cambia el color del texto a negro
+    titleElement.style.color = "black";
+  }
+  // Si el modo de la página es "dark-mode" y el texto actual es en inglés
+  if (bodyElement.classList.contains("dark-mode") && currentText === oraciones[id]["english"]) {
+    // Cambia el color del texto a blanco
+    titleElement.style.color = "blue";
+  } 
+  // Si el modo de la página es "dark-mode" y el texto actual es en inglés
+  if (bodyElement.classList.contains("dark-mode") && currentText === oraciones[id]["spanish"]) {
+    // Cambia el color del texto a blanco
+    titleElement.style.color = "white";
+  } 
+}
+// Objeto con las frases en inglés y español
+const oraciones = {
+  "titulo1": {
+    "english": "The musician plays a strange song on the piano under the stars during the summer.",
+    "spanish": "El músico toca una canción extraña en el piano bajo las estrellas durante el verano."
+  },
+  "titulo2": {
+    "english": "The scientist conducts experiments in the laboratory with sophisticated equipment.",
+    "spanish": "El científico realiza experimentos en el laboratorio con equipo sofisticado."
+  },
+  "titulo3": {
+    "english": "The child paints a colorful picture of a beautiful landscape.",
+    "spanish": "El niño pinta una imagen colorida de un hermoso paisaje."
+  },
+  "titulo4": {
+    "english": "The chef prepares a delicious meal with fresh ingredients.",
+    "spanish": "El chef prepara una deliciosa comida con ingredientes frescos."
+  },
+  "titulo5": {
+    "english": "She sings a beautiful song about love and happiness.",
+    "spanish": "Ella canta una hermosa canción sobre el amor y la felicidad."
+  },
+  "titulo6": {
+    "english": "The cat gracefully plays with a colorful toy.",
+    "spanish": "El gato juega graciosamente con un juguete colorido."
+  }
+};
+
+
+
+
+
+
+
+/* DARK MODE */
+document.getElementById('dark-mode-button').addEventListener('click', function() {
+  // Obtener la clase actual del elemento body
+  dark = document.querySelector("#darkIcon"); // se le cambiara el icono de dark a light unicamente cambiando el texto
+  var currentClass = document.body.classList.value;
+
+  const titulo = document.querySelectorAll(".titulo");
+  // Si el elemento body tiene la clase light-mode, cambiar a dark-mode
+  // De lo contrario, cambiar a light-mode
+  if (currentClass === 'light-mode') {
+    document.body.classList.remove('light-mode');
+    document.body.classList.add('dark-mode');
+    dark.textContent = "light_mode"
+  } else {
+    document.body.classList.remove('dark-mode');
+    document.body.classList.add('light-mode');
+    dark.textContent = "dark_mode"
+  }
+  colorTitulos()
+});
+
+/* Cada vez que se cambie de modo light o dark, los titulos cambiaran de color tambien en caso de haber cambiado sus colores previamente, se realizo esto para evitar un par de errores en donde el texto quedaba en white en algunos casos no deseados */
+const titulo = document.querySelectorAll(".titulo");// Obtener la lista de elementos con la clase "titulos"
+function colorTitulos(){
+  titulo.forEach(function(titulo) {   // Iterar sobre la lista de elementos
+  var currentClass = document.body.classList.value;
+  // Si el modo de la página es "light-mode"
+  if (currentClass =="light-mode") {
+    titulo.style.color = "black";
+  } 
+  else
+    {titulo.style.color = "white"}
+});
+
+}
+
+
+
+
+
+
+/* MENU DESPLEGABLE VERBOS CAMBIANDO UN DISPLAY DE NONE A BLOCK */
+document.querySelector(".dropdown-btn").addEventListener("click", function(e) {
+  // Prevenimos que el evento siga propagándose
+  e.stopPropagation();
+  // Obtenemos el elemento con clase "dropdown-content"
+  var dropdownContent = document.querySelector(".dropdown-content");
+  // Se evalua, esta en block, cambia a none, sino, a block
+  if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+  } else {
+    dropdownContent.style.display = "block";
+  }
+});
+
+/* AL HACER CLICK FUERA DEL MENU, ESTE SE CIERRA */
+document.querySelector("body").addEventListener("click", function(e) {
+  // Si el elemento clicado no es el mismo que el elemento "dropdown-btn" ni un descendiente del mismo
+  if (!e.target.closest(".dropdown-btn")) {
+    // Ocultamos el menú desplegable
+    document.querySelector(".dropdown-content").style.display = "none";
+  }
+});
+
+
+
---
+++ verbosGerundio.js
@@ -0,0 +1,214 @@
+/* Accediendo al index.html y cargando la funcion que accedera a la api */
+document.addEventListener('DOMContentLoaded', ()=>{ /* el evento "Domcontendloaded carga cuado la info esta totalmente cargada (la info de json)*/
+    fetchData() /* cargara la funcion flecha fetchdata */
+})
+
+
+let data;
+
+const fetchData = async () => {
+  try {
+    const res = await fetch('verbosGerundio.json');
+    data = await res.json();
+    pintarCards(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+contenedor = document.querySelector(".contenedor2")
+seccion1 = document.querySelector(".seccion1")
+cambiarIdioma= document.getElementById("cambiarIdioma")
+
+
+let idiomaActual = "español"; // Asignamos el idioma actual
+
+
+  //boton para cambiar el idioma
+  let botonCambiarIdioma = document.createElement("button");
+  botonCambiarIdioma.textContent = "Español";
+  botonCambiarIdioma.id= "botonCambiarIdioma-id"
+  seccion1.appendChild(botonCambiarIdioma);
+
+
+  botonCambiarIdioma.addEventListener("click", ()=> {
+    // Cambia el valor de idiomaActual a "español" o "ingles" según corresponda
+    
+    if (idiomaActual === "ingles") {
+      idiomaActual = "español";
+      botonCambiarIdioma.textContent = "Español";
+    } else {
+      idiomaActual = "ingles";
+      botonCambiarIdioma.textContent = "Ingles";
+    }
+    // Borra todos los elementos actuales del contenedor
+    contenedor.textContent = "";
+    // Vuelve a llamar a la función pintarCards con los datos actualizados
+    pintarCards(data, idiomaActual);
+  });
+
+
+
+/* Usado para activar o desactivar el audio desde el usuario */
+
+let audioEnabled = false; //Variable global que indica si el audio está habilitado o no
+const toggleAudioButton = document.getElementById("toggle-audio-button"); 
+
+toggleAudioButton.addEventListener("click", () => {
+  const audio= document.getElementById("audio")
+  audioEnabled = !audioEnabled; // Invertira el valor de la variable booleana al ocurrir el evento click
+  if (audioEnabled) {
+    audio.textContent = "volume_up";
+  } else {
+    audio.textContent = "volume_off";
+  }
+});
+
+
+//Variable para almacenar la voz en inglés
+let englishVoice;
+
+//Función para buscar una voz en inglés en el dispositivo
+window.speechSynthesis.onvoiceschanged = () => {
+  const voices = window.speechSynthesis.getVoices();
+  englishVoice = voices.find(voice => voice.lang === 'en-US');
+};
+
+//Función para reproducir el audio en inglés, el llamado a la funcion se realiza al hacer click en alguna palabra, desde la funcion pintarCard
+function speak(text) {
+  if ('speechSynthesis' in window) { //si es viable usar speechSynthesis
+      if (audioEnabled) { //en caso que el audio este activado desde el usuario
+        const locutor = new SpeechSynthesisUtterance();
+        locutor.text = text;
+        locutor.voice = englishVoice;
+        locutor.rate = 0.65; //velocidad audio
+        locutor.pitch = 1.1; //tono de voz
+        window.speechSynthesis.speak(locutor);
+      } else {
+        console.log("El audio no esta habilitado");
+      }
+  } else { //en caso que el dispositivo no pueda usar speechSynthesis
+    console.log("El navegador no soporta speechSynthesis");
+  }
+}
+
+
+
+//Función principal para pintar todas las palabras desde json
+pintarCards = (data, idiomaActual) => { 
+  for (let i = 0; i < data.length; i++) {
+
+    let palabra = document.createElement("div");
+
+    if (idiomaActual === "ingles") { 
+      palabra.textContent = data[i].español.join(", "); 
+      palabra.id = "español-id"
+    } else { //como idiomaActual es en "español", se activara solo el else y se pintaran las palabras en ingles
+      palabra.textContent = data[i].ingles;  
+      palabra.id = "ingles-id"  
+    }
+    palabra.classList.add("palabra");
+
+
+    //Evento click para cambiar el idioma y reproducir el audio
+    palabra.addEventListener("click", (e) => {
+      palabra.classList.toggle("click");
+      if (palabra.textContent === data[i].ingles) {
+        palabra.textContent = data[i].español.join(", ");
+      } else {
+        palabra.textContent = data[i].ingles;
+      }
+      speak(data[i].ingles); //llamado a la funcion para generar el audio en la palabra concreta a la que se clickea
+    });
+
+    contenedor.appendChild(palabra); 
+  }
+};
+
+
+/* Usado para generar el menu responsivo */
+const checkbox = document.querySelector('#check');
+const elUl = document.getElementById("elUl")
+
+checkbox.addEventListener('click', () => {
+  // Si el checkbox está marcado, agregamos la clase "active" al label
+  if (checkbox.checked) {
+    elUl.style.overflow = "auto"; /* overflow usado para poder scrolear en el menu responsive, */
+    document.body.style.overflow = 'hidden'; /* overflow usado para no poder scrollear la pagina body mientras esta activo el menu resposive, o mas bien mientras este con el check el checkbox */
+    
+    elUl.classList.add('active');
+  } else {
+    // Si no está marcado, quitamos la clase "active" del label
+    elUl.classList.remove('active');
+    document.body.style.overflow = 'auto';
+    
+  }
+  document.getElementById("myBtn").style.display = "none"; //cuando se despliega el menu, el btn para subir desaparece
+});
+
+
+
+/* DARK MODE */
+document.getElementById('dark-mode-button').addEventListener('click', function() {
+  // Obtener la clase actual del elemento body
+  dark = document.querySelector("#darkIcon"); // se le cambiara el icono de dark a light unicamente cambiando el texto
+  var currentClass = document.body.classList.value;
+
+  // Si el elemento body tiene la clase light-mode, cambiar a dark-mode
+  // De lo contrario, cambiar a light-mode
+  if (currentClass === 'light-mode') {
+    document.body.classList.remove('light-mode');
+    document.body.classList.add('dark-mode');
+    dark.textContent = "light_mode"
+  } else {
+    document.body.classList.remove('dark-mode');
+    document.body.classList.add('light-mode');
+    dark.textContent = "dark_mode"
+  }
+});
+
+
+
+
+ // Cuando se scrollea 20px por debajo de la parte superior, se muestra el boton automaticamente, de lo contrario no se ve
+ window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("myBtn").style.display = "block";
+    } else {
+      document.getElementById("myBtn").style.display = "none";
+    }
+ }
+
+ function topFunction() { 
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'/* usada unicamente para que el desplazamiento hacia la parte superior sea mas suave */
+  });
+}
+
+
+
+/* MENU DESPLEGABLE VERBOS CAMBIANDO UN DISPLAY DE NONE A BLOCK */
+document.querySelector(".dropdown-btn").addEventListener("click", function(e) {
+  // Prevenimos que el evento siga propagándose
+  e.stopPropagation();
+  // Obtenemos el elemento con clase "dropdown-content"
+  var dropdownContent = document.querySelector(".dropdown-content");
+  // Se evalua, esta en block, cambia a none, sino, a block
+  if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+  } else {
+    dropdownContent.style.display = "block";
+  }
+});
+/* AL HACER CLICK FUERA DEL MENU, ESTE SE CIERRA */
+document.querySelector("body").addEventListener("click", function(e) {
+  // Si el elemento clicado no es el mismo que el elemento "dropdown-btn" ni un descendiente del mismo
+  if (!e.target.closest(".dropdown-btn")) {
+    // Ocultamos el menú desplegable
+    document.querySelector(".dropdown-content").style.display = "none";
+  }
+});
